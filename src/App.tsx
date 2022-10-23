867169cc28a9db5e31dd9b05eb1ba3fdddf4bb75
DevelopJKong <ybaram40@gmail.com>
2022-10-23 15:31:11 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button``;
+
+const Text = styled.div``;
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const onUpNumber = () => {
+    setNumber((current) => current + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Text>{number}</Text>
+      <Button onClick={onUpNumber}>클릭</Button>
     </div>
   );
 }
